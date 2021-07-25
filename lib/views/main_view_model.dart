76f8ecdcb4b55e40984c41a9fe8631d8d7206bc5
cygnu <crucia8@icloud.com>
2021-07-26 01:10:41 +0900

    fix: Changed package names to plural

--- lib/views/main_view_model.dart
+++ lib/views/main_view_model.dart
@@ -1,19 +1,19 @@
 import 'package:scanner/models/google_books_api_service.dart';
-import 'package:scanner/view/main_view_model_data.dart';
+import 'package:scanner/views/main_view_model_data.dart';
 import 'package:state_notifier/state_notifier.dart';
 
 class MainViewModel extends StateNotifier<MainViewModelData> {
-  MainViewModel() : super(MainViewModelData);
+  MainViewModel() : super(MainViewModelData());
 
   void fetch(String keyword) {
     state = state.copyWith(viewModelState: MainViewModelState.loading);
 
     getBooks(keyword).then((res) {
       state = state.copyWith(
-          response: res, mainModelState: MainViewModelState.normal);
+          response: res, viewModelState: MainViewModelState.normal);
     }).catchError((_) {
       state = state.copyWith(
-          response: null, mainModelState: MainViewModelState.error);
+          response: null, viewModelState: MainViewModelState.error);
     });
   }
 }
