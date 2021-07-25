de6e1487d8074b016297a9108494bf7509cf79ff
cygnu <crucia8@icloud.com>
2021-07-25 16:00:07 +0900

    feat: Implement view/main_view_model*.dart

---
+++ lib/view/main_view_model_data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:scanner/models/google_books_response.dart';
+
+part 'main_view_model_data.freezed.dart';
+
+enum MainViewModelState { normal, loading, error }
+
+@freezed
+class MainViewModelData with _$MainViewModelData {
+  const MainViewModelData._();
+  const factory MainViewModelData({
+    required GoogleBooksResponse response,
+    required MainViewModelState viewModelState,
+  }) = _MainViewModelData;
+  factory MainViewModelData.fromJson(Map<String, dynamic> json) =>
+      _$MainViewModelDataFromJson(json);
+}
