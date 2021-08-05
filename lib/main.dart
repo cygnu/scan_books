23dea3461edc02f4adee4aa3cb7e94170461d802
cygnu <crucia8@icloud.com>
2021-08-06 06:31:23 +0900

    feat: Define the routing 'DetailsScreen'

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:scanner/navigation_navigator.dart';
 import 'package:scanner/screens/bookmarks_screen.dart';
+import 'package:scanner/screens/details_screen.dart';
 import 'package:scanner/screens/overview_screen.dart';
 
 void main() => runApp(
@@ -18,6 +19,7 @@ class ScanApp extends StatelessWidget {
       routes: {
         '/': (context) => NavigationNavigator(),
         OverviewScreen.route: (context) => OverviewScreen(),
+        DetailsScreen.route: (context) => DetailsScreen(),
         BookmarksScreen.route: (context) => BookmarksScreen(),
       },
     );
--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
+  static const route = 'details_screen';
+
   @override
   Widget build(BuildContext context) {
     return Container(
