39bc6ca448d09e506e6b8552d77532218225e2a0
cygnu <crucia8@icloud.com>
2021-08-07 02:03:24 +0900

    feat: Implement UI, display thumbnails on the details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -9,8 +9,27 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(book.volumeInfo.title),
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 45.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 1.5,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(book.volumeInfo.imageLinks.thumbnail),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
