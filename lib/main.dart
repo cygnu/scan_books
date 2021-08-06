7eb677083cf912f3a748d699d5e0a3d1c32149d2
cygnu <crucia8@icloud.com>
2021-08-06 13:00:30 +0900

    feat: Implemented to move to 'DetailsScreen' when tapped

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:scanner/models/google_book_response.dart';
 import 'package:scanner/navigation_navigator.dart';
 import 'package:scanner/screens/bookmarks_screen.dart';
 import 'package:scanner/screens/details_screen.dart';
@@ -12,6 +13,8 @@ void main() => runApp(
     );
 
 class ScanApp extends StatelessWidget {
+  late final GoogleBookResponse book;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +22,9 @@ class ScanApp extends StatelessWidget {
       routes: {
         '/': (context) => NavigationNavigator(),
         OverviewScreen.route: (context) => OverviewScreen(),
-        DetailsScreen.route: (context) => DetailsScreen(),
+        DetailsScreen.route: (context) => DetailsScreen(
+              book: book,
+            ),
         BookmarksScreen.route: (context) => BookmarksScreen(),
       },
     );
--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:scanner/models/google_book_response.dart';
 
 class DetailsScreen extends StatelessWidget {
   static const route = 'details_screen';
 
+  const DetailsScreen({Key? key, required this.book}) : super(key: key);
+  final GoogleBookResponse book;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Details'),
+      child: Text(book.volumeInfo.title),
     );
   }
 }
