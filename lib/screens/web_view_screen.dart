7a1985fbb9db5bb936771443fef973dff137f8d0
cygnu <crucia8@icloud.com>
2021-08-08 12:42:08 +0900

    refactor: Implement screens/web_view_screen.dart

---
+++ lib/screens/web_view_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WebViewScreen extends StatefulWidget {
+  @override
+  _WebViewScreenState createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('webview'),
+      ),
+    );
+  }
+}
