2e261a5f3167e4b27d82fa686a101854fab8b18e
cygnu <crucia8@icloud.com>
2021-08-14 15:11:43 +0900

    refactor: Implement components/book_item.dart

--- lib/components/book_item.dart
+++ lib/components/book_item.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:scanner/models/google_book_response.dart';
+import 'package:scanner/screens/details_screen.dart';
 
+class BookItem extends StatelessWidget {
+  const BookItem({Key? key, required this.book}) : super(key: key);
+  final GoogleBookResponse book;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.network(
+        book.volumeInfo.imageLinks!.thumbnail!,
+      ),
+      title: Text(
+        book.volumeInfo.title,
+        maxLines: 2,
+      ),
+      subtitle: Text(
+        book.volumeInfo.description != null ? book.volumeInfo.description! : '',
+        maxLines: 2,
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailsScreen(book: book),
+          ),
+        );
+      },
+    );
+  }
+}
