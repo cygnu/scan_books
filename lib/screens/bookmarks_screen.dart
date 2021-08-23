41ba8fefa0e7e8218c74bb1b367a4c9906792f5e
cygnu <crucia8@icloud.com>
2021-08-23 14:18:13 +0900

    fix: Implement 'flutter_riverpod' in screens/bookmarks_screen.dart

--- lib/screens/bookmarks_screen.dart
+++ lib/screens/bookmarks_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:scanner/models/google_book_response.dart';
+import 'package:scanner/navigation_navigator.dart';
 
-class BookmarksScreen extends StatelessWidget {
+class BookmarksScreen extends ConsumerWidget {
   static const route = 'bookmarks_screen';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final state = watch(viewModel);
+    final List<GoogleBookResponse> bookList =
+        state.response != null ? state.response!.items : [];
+
     return Container(
       child: Text('bookmarks'),
     );
