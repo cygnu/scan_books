e1b2ebb3ea9f1bbfe6fcf1a218e10a29a3a3889a
cygnu <crucia8@icloud.com>
2021-09-13 00:52:14 +0900

    fix: Remove 'Expanded' widget

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -15,71 +15,68 @@ class DetailsScreen extends StatelessWidget {
         title: Text(book.volumeInfo.title),
       ),
       body: Container(
-        child: Expanded(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 10.0, vertical: 10.0),
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 1.5,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image:
-                          NetworkImage(book.volumeInfo.imageLinks!.thumbnail!),
-                      fit: BoxFit.fitHeight,
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 1.5,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(book.volumeInfo.imageLinks!.thumbnail!),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      book.volumeInfo.title,
+                      maxLines: 2,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ),
-              ),
-              Flexible(
-                child: Container(
-                  padding: EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        book.volumeInfo.title,
-                        maxLines: 2,
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Material(
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Colors.blue,
-                            child: MaterialButton(
-                              child: Text(
-                                'More at web',
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        WebViewScreen(book: book),
-                                  ),
-                                );
-                              },
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Material(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.blue,
+                          child: MaterialButton(
+                            child: Text(
+                              'More at web',
+                              style: const TextStyle(color: Colors.white),
                             ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      WebViewScreen(book: book),
+                                ),
+                              );
+                            },
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
